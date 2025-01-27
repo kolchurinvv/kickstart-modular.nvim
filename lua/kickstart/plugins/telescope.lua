58921f4e0e4b199effbc9e9b22fd48adf278d0b2
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2025-01-27 18:52:52 +0100

    telescope - hide .git

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -64,6 +64,7 @@ return {
         defaults = {
           file_ignore_patterns = {
             'node_modules',
+            '.git',
             'build',
             'dist',
             '*.lock',
