ae1856f1c74e30a81f1fb737a234ccb4f6bfd8e9
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2025-08-06 20:15:55 +0200

    noice - floating messages + command line; mongosh - client UI for mongo

---
+++ lua/custom/plugins/mongosh-nvim.lua
@@ -0,0 +1,18 @@
+return {
+  'donus3/mongo.nvim',
+  dependencies = {
+    'ibhagwan/fzf-lua',
+  },
+  opts = {
+    ---default mongo url show on start up
+    default_url = 'mongodb://0.0.0.0:27017',
+    ---execute query on collection selected
+    find_on_collection_selected = false,
+    ---mongo binary path for mongodb < 3.6
+    mongo_binary_path = '/etc/profiles/per-user/_liminor/bin/mongo',
+    ---mongodb shell binary path
+    mongosh_binary_path = '/etc/profiles/per-user/_liminor/bin/mongosh',
+    ---number of documents in the result
+    batch_size = 100,
+  },
+}
